e32fd493f26ccbbd2297677ef4113065853b1ac6
biapolegatti <bianca.polegatti@gmail.com>
2022-05-09 14:16:41 -0300

    primeiro commit.

---
+++ src/main/java/model/ClienteRepostiory.java
@@ -0,0 +1,6 @@
+package model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepostiory extends CrudRepository <Cliente, Long> {
+}
---
+++ src/main/java/service/ViaCepService.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "viacep.com.br/ws/01001000/json/")
+public interface ViaCepService {
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
+
+
+
+}
